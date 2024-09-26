168be39864e0b4810578deaf04d4528c01921a4d
CollisionDotNet <collisiondotnet@mail.ru>
2024-09-26 23:27:01 +0300

    Тип Image теперь ValueObject, а не Entity

---
+++ Contacts.Domain/ValueObjects/Image.cs
@@ -0,0 +1,26 @@
+﻿using Contacts.Domain.Exceptions;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Contacts.Domain.ValueObjects
+{
+    public class Image : ValueObject
+    {        
+        public Uri Uri { get; }
+        public Image(Uri uri)
+        {
+            StringBuilder validationErrors = new StringBuilder();
+            if (uri == null)
+                validationErrors.Append($"{nameof(Uri)} can't be null");
+
+            if (validationErrors.Length > 0)
+                throw new ValidationException(validationErrors.ToString());
+
+            Uri = uri!;
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Uri;
+        }
+    }
+}

121efff1f1352e3943173596bd754bdfd02705ca
CollisionDotNet <collisiondotnet@mail.ru>
2024-09-26 23:37:38 +0300

    Определены абстракции репозитория и сервиса для Contact на уровне Domain
    * Добавлен интерфейс репозитория Contact IContactRepository
    * Добавлен интерфейс сервиса Contact IContactService

---
+++ Contacts.Domain/Abstractions/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contacts.Domain.Entities;
+
+namespace Contacts.Domain.Abstractions
+{
+    public interface IContactRepository
+    {
+        public Task<int> CreateAsync(Contact contact);
+        public Task<Contact> GetAsync(int id);
+        public Task<IEnumerable<Contact>> GetAllAsync();
+        public Task<int> UpdateAsync(int id, Contact contact);
+        public Task<int> DeleteAsync(int id);      
+    }
+}
---
+++ Contacts.Domain/Abstractions/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using Contacts.Domain.Entities;
+
+namespace Contacts.Domain.Abstractions
+{
+    public interface IContactService
+    {
+        public Task<int> CreateContactAsync(Contact contact);
+        public Task<Contact> GetContactAsync(int id);
+        public Task<IEnumerable<Contact>> GetAllContactsAsync();
+        public Task<int> UpdateContactAsync(int id, Contact contact);
+        public Task<int> DeleteContactAsync(int id);
+
+    }
+}

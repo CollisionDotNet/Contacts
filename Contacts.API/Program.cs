35a66415f042bf69f3248da5b7ac9a7b78280bdd
CollisionDotNet <collisiondotnet@mail.ru>
2024-09-28 03:36:39 +0300

    Зарегистрированы сервисы на уровне API
    * ContactsDbContext добавлен в сервисы как контекст для работы с БД с соответствующей строкой подключения
    * Реализация IContactService в виде ContactService добавлена в сервисы через DI
    * Реализация IContactRepository в виде EFContactRepository добавлена в сервисы через DI

--- Contacts.API/Program.cs
+++ Contacts.API/Program.cs
@@ -1,4 +1,8 @@
-
+using Contacts.Application.Services;
+using Contacts.Domain.Abstractions;
+using Contacts.Infrastructure;
+using Contacts.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 namespace Contacts.API
 {
     public class Program
@@ -10,7 +14,14 @@ namespace Contacts.API
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddScoped<IContactService, ContactService>();
+            builder.Services.AddScoped<IContactRepository, EFContactRepository>();
+            string? contactsDBConnectionString = builder.Configuration.GetConnectionString(nameof(ContactsDbContext));
+            builder.Services.AddDbContext<ContactsDbContext>(options =>
+            {
+                options.UseSqlServer(contactsDBConnectionString);
+            });
+            
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
